47d43433d15ef1b82b8d6e8f8276c764e7c88f83
nikhiludas <nikhiludas38@gmail.com>
2026-03-27 09:13:20 +0545

    deplo fixes

--- src/models/index.ts
+++ src/models/index.ts
@@ -8,7 +8,10 @@ if (!connectionString) {
   throw new Error('DATABASE_URL environment variable is not set');
 }
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ 
+  connectionString,
+  ssl: connectionString.includes('localhost') ? false : { rejectUnauthorized: false }
+});
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({ adapter });
